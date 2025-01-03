17e99d8cb1a5f5e63d14a2909dde171512eb2cc6
nitishdhingra29 <nitish98dhingra@gmail.com>
2025-01-04 03:02:41 +0530

    Initial commit

---
+++ src/main/java/org/example/Addition.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Addition {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Addition addition=new Addition();
+        System.out.println(addition.add(2,4));
+    }
+}
